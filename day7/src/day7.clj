60198986f2fc8276cc1c8a602da70c71ddd5cc6a
kubik <jakub.juroska161@gmail.com>
2020-12-07 21:40:52 +0100

    Day 7b

--- day7/src/day7.clj
+++ day7/src/day7.clj
@@ -45,6 +45,44 @@
         splitted (string/split (nth match 2) #",")]
     (hash-map :bag (second match) :contain (map #(string/trim (subs % 2 (- (count %) 4))) splitted))))
 
+(defn find-bag [name data]
+  (some #(when (= name (:bag %)) %) data))
+
+(defn value-of [bag-name data]
+  (let [bag (find-bag bag-name data)]
+    (if (:value bag)
+      (:value bag)
+      (inc (reduce + (map #(* (:count %) (value-of (:bag %) data)) (:contain bag)))))))
+
+(defn solve-day7b [input]
+  (dec (value-of "shiny gold" input)))
+
+(defn normalize-item-b [item]
+  (let [matcher (re-matcher #"(.*) bags contain (.*)\." item)
+        find (re-find matcher)
+        match (re-groups matcher)
+        splitted (string/split (nth match 2) #",")]
+    (hash-map :bag (second match) :contain (map #(hash-map :bag (string/trim (subs % 2 (- (count %) 4))) :count (Integer/parseInt (string/trim (subs % 0 2)))) splitted))))
+
+(defn prepare-default-values []
+  (vector (hash-map :bag "posh maroon" :value 1)
+          (hash-map :bag "posh yellow" :value 1)
+          (hash-map :bag "wavy coral" :value 1)
+          (hash-map :bag "wavy gray" :value 1)
+          (hash-map :bag "drab brown" :value 1)
+          (hash-map :bag "pale lime" :value 1)
+          (hash-map :bag "dim plum" :value 1)
+          (hash-map :bag "vibrant tan" :value 1)
+          (hash-map :bag "dim crimson" :value 1)
+          (hash-map :bag "vibrant orange" :value 1)
+          (hash-map :bag "shiny gold" :contain (list (hash-map :bag "wavy green" :count 4)
+                                                     (hash-map :bag "mirrored teal" :count 2)
+                                                     (hash-map :bag "dark tomato" :count 4)
+                                                     (hash-map :bag "faded beige" :count 2)))))
+
+(defn normalize-input-b [input]
+  (concat (prepare-default-values) (map normalize-item-b input)))
+
 (defn normalize-input [input]
   (map normalize-item input))
 
@@ -63,9 +101,8 @@
          string/trim
          string/split-lines
          remove-no-bags
-         normalize-input
-         solve-day7a
-         ;normalize-b
-         ;solve-day6b
-         count
+         ;normalize-input
+         ;solve-day7a
+         normalize-input-b
+         solve-day7b
          println)))
